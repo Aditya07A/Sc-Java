a8309c7800708d274ce2d86c0d6ca1eb3ffa8914
Aditya <adityaadiraj74@gmail.com>
2023-09-25 20:38:19 +0530

    harry

--- Hello.java
+++ Hello.java
@@ -1,10 +1,16 @@
 
+import java.util.Scanner;
 
-class HelloWorld {
-	// Your program begins with a call to main().
-	// Prints "Hello, World" to the terminal window.
-	public static void main(String args[])
-	{
-		System.out.println("Hello, World");
+class Hello {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter num1: ");
+		int a = sc.nextInt();
+		System.out.println("enter num2: ");
+		int b = sc.nextInt();
+		int sum = a + b;
+		System.out.println("sum is : ");
+		System.out.println(sum);
 	}
 }
