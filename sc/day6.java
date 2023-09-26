0d47a1527316548af769e7ac741c4272c5dc43e4
Aditya <adityaadiraj74@gmail.com>
2023-09-26 19:04:45 +0530

    day6

---
+++ sc/day6.java
@@ -0,0 +1,54 @@
+package sc;
+
+import java.util.Scanner;
+
+public class day6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // question 1 ---------------------------
+        // System.out.println("Hello 40 ");
+        // System.out.println("50 World!");
+
+        // question 2 ---------------------------
+        // System.out.println("Hello 40 ");
+        // System.out.println("\n ");
+        // System.out.println("50 World!");
+
+        // question 3 ---------------------------
+        // System.out.println("Hello ");
+        // System.out.println("World !");
+
+        // question 4 ---------------------------
+        // int a = 10;
+        // int b = 20;
+        // int c = 30;
+        // System.out.print(a);
+        // System.out.println(b);
+        // System.out.print(c);
+        /*
+         * o/p=>1020
+         * =>30
+         */
+        /*
+         * question 5 ---------------------------
+         * int g = 9;
+         * System.out.println(g * 3);
+         * o/p=> 27
+         */
+        /*
+         * question 6 ---------------------------
+         * option (b) int a = 10; System.out.print(a);
+         */
+        /*
+         * question 7 ---------------------------
+         * option (c) System.out.println(1020);
+         */
+
+        /*
+         * question 8 ---------------------------
+         * option (c)Semi-colon [ ; ]
+         */
+
+        sc.close();
+    }
+}
