b414933d77078a11a9c8a5db9888ee4f3cf7fc72
Aditya <adityaadiraj74@gmail.com>
2023-09-26 17:22:00 +0530

    update

---
+++ day5.java
@@ -0,0 +1,67 @@
+public class day5 {
+    public static void main(String[] args) {
+        // Print the first five letters of the English alphabet i.e. A, B, C, D and E.
+
+        /*
+         * System.out.print("A");
+         * System.out.print("B");
+         * System.out.print("C");
+         * System.out.print("D");
+         * System.out.print("E");
+         */
+        /*
+         * Perform the following operations on 6 and 3 and print their respective
+         * outputs in different lines.
+         * 1. Addition
+         * 2. Subtraction
+         * 3. Multiplication
+         * 4. Division
+         */
+        /*
+         * System.out.print(6 + 3);
+         * System.out.print("\n");
+         * System.out.print(6 - 3);
+         * System.out.print("\n");
+         * System.out.print(6 * 3);
+         * System.out.print("\n");
+         * System.out.print(6 / 3);
+         */
+        /*
+         * Print the following pattern in output
+         ***** 
+         **** 
+         *** 
+         ** 
+         * 
+         * 
+         * 
+         */
+
+        /*
+         * System.out.println("*****");
+         * System.out.println("****");
+         * System.out.println("***");
+         * System.out.println("**");
+         * System.out.println("*");
+         */
+        /*
+         * Print the following pattern in output
+         *
+         **
+         ***
+         ****
+         *****
+         * 
+         */
+        /*
+         * System.out.println("    *");
+         * System.out.println("   **");
+         * System.out.println("  ***");
+         * System.out.println(" ****");
+         * System.out.println("*****");
+         */
+        // Print "Hello World !" in the output.
+        System.out.println("Hello World !");
+
+    }
+}
