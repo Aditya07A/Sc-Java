6768e8a4fd3af430c95f5da1622c14369abc03cb
Aditya <adityaadiraj74@gmail.com>
2023-09-26 15:33:19 +0530

    practiceset1

---
+++ cwh_practice1.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class cwh_practice1 {
+    public static void main(String[] args) {
+        // System.out.println("hello");
+        // question 1---------------------------------------
+        Scanner sc = new Scanner(System.in);
+        // int num1 = sc.nextInt();
+        // int num2 = sc.nextInt();
+        // int num3 = sc.nextInt();
+        // int sum = num1 + num2 + num3;
+        // System.out.println(sum);
+        // question 2------------------------------------------
+        // float num1 = sc.nextInt();
+        // float num2 = sc.nextInt();
+        // float num3 = sc.nextInt();
+        // float avg = num1 + num2 + num3;
+        // System.out.println(avg);
+        // float cgpa = avg / 30;
+        // System.out.println(cgpa);
+        // question 3----------------------------------------------
+        // String name = sc.nextLine();
+        // System.out.println("Hello," + name + " have a good day");
+        // question 4---------------------------------------------
+        // System.out.println("Enter no.of kilometers :");
+        // double kilometer = sc.nextDouble();
+        // double miles = kilometer * 0.621371;
+        // System.out.println(miles);
+        // question 5---------------------------------------------
+        System.out.println("Enter number: ");
+        System.out.println(sc.hasNextInt());
+        sc.close();
+    }
+}
