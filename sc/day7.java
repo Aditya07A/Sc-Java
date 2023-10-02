f99c5941c7e0475cfc30015d15abb21daad4dd35
Aditya <adityaadiraj74@gmail.com>
2023-10-02 19:44:34 +0530

    day7

---
+++ sc/day7.java
@@ -0,0 +1,69 @@
+package sc;
+
+import java.util.Scanner;
+
+public class day7 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        // question--1-------------------------------------------
+        // int a = 10, b = 5; // 1
+        // int c = (a * 1.0) / b; // 2
+        // System.out.println(c); // 3
+
+        /*
+         * // question-2--------------------------------------
+         * int N = sc.nextInt();
+         * int time = 40 - N;
+         * System.out.println(time);
+         */
+
+        /*
+         * // question-3--------------------------------------
+         * int N = sc.nextInt();
+         * int min = N * 2;
+         * System.out.println(min);
+         */
+        /*
+         * // question-4--------------------------------------
+         * int N = sc.nextInt();
+         * int M = sc.nextInt();
+         * int out = M + (N * 2);
+         * System.out.println(out);
+         */
+        /*
+         * int A = sc.nextInt();
+         * int B = sc.nextInt();
+         * int C = sc.nextInt();
+         * int sum = A + B + C;
+         * System.out.println(sum);
+         */
+        /*
+         * // question-6--------------------------------------
+         * int x = 10;
+         * x = x * 5;
+         * System.out.println(x);
+         * 
+         */
+        // question-7--(c)The type or variety of data being handled for reading and
+        // writing------------------------------------
+
+        /*
+         * // question-8---(b)--int a = 20, int b = 30;
+         * / question-9---(b)----------------------------
+         * float a = 5 / 2;
+         * System.out.println(a);// 2.0
+         */
+        // question-10--(b)bool--------
+        /*
+         * // question-11----------
+         * float a = 35f / 0;
+         * System.out.println(a);// infinity
+         */
+        // question-12--(a)line1--------
+        // question-13--(c)true--------
+
+        sc.close();
+
+    }
+}
