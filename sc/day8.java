3a08492b3f98ffa695d4a9494bbae6e290e8b370
Aditya <adityaadiraj74@gmail.com>
2023-11-26 12:22:19 +0530

    complete day 8

--- sc/day8.java
+++ sc/day8.java
@@ -30,14 +30,20 @@ public class day8 {
         // System.out.println("(n1 < n2) && (n2 == n3) condition is " + ((n1 < n2) &&
         // (n2 == n3)));
         // question 3
-        int n1 = sc.nextInt();
-        int n2 = sc.nextInt();
-        int n3 = sc.nextInt();
-        System.out.println("Number 1 is " + n1);
-        System.out.println("Number 2 is " + n2);
-        System.out.println("Number 3 is " + n3);
-        System.out.println("Number 3 is " + n3);
-        System.out.println((n1 < 50) || (n2 > n3) || (n1 == n3));
+        // int n1 = sc.nextInt();
+        // int n2 = sc.nextInt();
+        // int n3 = sc.nextInt();
+        // System.out.println("Number 1 is " + n1);
+        // System.out.println("Number 2 is " + n2);
+        // System.out.println("Number 3 is " + n3);
+        // System.out.println("Number 3 is " + n3);
+        // System.out.println((n1 < 50) || (n2 > n3) || (n1 == n3));
+        // question 4
+        // String S = sc.nextLine();
+        // int N = sc.nextInt();
+        // System.out.println(N +" "+ S);
+        // question 5
+
         sc.close();
     }
 }
