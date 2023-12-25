277308dd74e5b78d7a4e979565740c1bf153faba
Aditya <adityaadiraj74@gmail.com>
2023-12-25 22:11:04 +0530

    q1

---
+++ sc/day13.java
@@ -0,0 +1,25 @@
+package sc;
+
+import java.util.*;
+
+public class day13 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // q1-Assignment--
+        // int a = sc.nextInt();
+        // int i = 1;
+        // while ((i * i) <= a) {
+        // System.out.print(i * i + " ");
+        // i++;
+        // }
+        // q2-Assignment--
+        // long N = sc.nextLong();
+        // long M = sc.nextLong();
+        // int i = 0;
+        // while (i < N) {
+        // System.out.print(i * i + " ");
+        // i++;
+        // }
+        sc.close();
+    }
+}
