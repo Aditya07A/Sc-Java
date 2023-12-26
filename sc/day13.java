07e09bb094febb49b1554f37fc51e57db5a3dfbb
Aditya <adityaadiraj74@gmail.com>
2023-12-26 17:01:59 +0530

    q3

--- sc/day13.java
+++ sc/day13.java
@@ -50,21 +50,74 @@ public class day13 {
         // T--;
         // }
         // q4-Assignment--
-        int N = sc.nextInt();
-        int x = 0;
-        int y = 0;
-        while (N > 0) {
-            int ld = N % 10;
-            N = N / 10;
-            if (ld % 2 == 0) {
-                x += ld;
-            } else {
-                y += ld;
-            }
-        }
-        System.out.println(x);
-        System.out.println(y);
+        // int N = sc.nextInt();
+        // int x = 0;
+        // int y = 0;
+        // while (N > 0) {
+        // int ld = N % 10;
+        // N = N / 10;
+        // if (ld % 2 == 0) {
+        // x += ld;
+        // } else {
+        // y += ld;
+        // }
+        // }
+        // System.out.println(x);
+        // System.out.println(y);
 
+        // q5-Assignment--
+        // int A = sc.nextInt();
+        // int B = sc.nextInt();
+        // int i = A;
+        // while (i <= B) {
+        // System.out.print(i + " ");
+        // i++;
+        // }
+        // q1-Add--
+        // int T = sc.nextInt();
+        // while (T > 0) {
+        // int N = sc.nextInt();
+        // int sum = 0;
+        // while (N > 0) {
+        // int ld = N % 10;
+        // sum += ld;
+        // N /= 10;
+        // }
+        // System.out.println(sum);
+        // }
+        // T--;
+        // q2-Add--
+        // int N = sc.nextInt();
+        // int i = 1;
+        // while (i <= N) {
+        // int j = i;
+        // int sum = 0;
+        // while (j > 0) {
+        // int ld = j % 10;
+        // sum += (ld * ld * ld);
+        // j /= 10;
+        // }
+        // if (sum == i) {
+        // System.out.println(i);
+        // }
+        // i++;
+        // }
+        // q3-Add--
+        int T = sc.nextInt();
+        while (T > 0) {
+            int N = sc.nextInt();
+            int ld = 0;
+            int fd = 0;
+            ld = N % 10;
+            while (N > 0) {
+                fd = N % 10;
+                N = N / 10;
+
+            }
+            System.out.println(fd + " " + ld);
+
+        }
+        T--;
         sc.close();
     }
 }
