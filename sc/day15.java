cef51c1deb97a687e8bffb9bc24c12784c502f6d
Aditya <adityaadiraj74@gmail.com>
2023-12-28 22:16:26 +0530

    q3

--- sc/day15.java
+++ sc/day15.java
@@ -15,19 +15,48 @@ public class day15 {
         // }
         // System.out.println(count);
         // q2 Assignment ---
-        int a = sc.nextInt();
-        int count = 0;
-        for (int i = 1; i <= a; i++) {
-            if (a % i == 0) {
-                count++;
+        // int a = sc.nextInt();
+        // int count = 0;
+        // for (int i = 1; i <= a; i++) {
+        // if (a % i == 0) {
+        // count++;
+        // }
+        // }
+        // if (count == 2) {
+        // System.out.println("yes");
+        // } else {
+        // System.out.println("no");
+        // }
+        // int a = sc.nextInt();
+        // boolean flag = true;
+        // for (int i = 2; i < a; i++) {
+        // if (a % i == 0) {
+        // flag = false;
+        // break;
+        // }
+        // }
+        // if (flag == true) {
+        // System.out.println("yes");
+        // } else {
+        // System.out.println("no");
+        // }
+        // q3 Assignment ---
+        int T = sc.nextInt();
+        while (T > 0) {
+            int n = sc.nextInt();
+            int sum = 0;
+            for (int i = 1; i < n; i++) {
+                if (n % i == 0) {
+                    sum += i;
+                }
             }
+            if (sum == n) {
+                System.out.println("yes");
+            } else {
+                System.out.println("no");
+            }
+            T--;
         }
-        if (count == 2) {
-            System.out.println("yes");
-        } else {
-            System.out.println("no");
-        }
-
         sc.close();
     }
 }
