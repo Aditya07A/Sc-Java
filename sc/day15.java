730eb8216eb238c4fd8f91870b77d4e54b391c9e
Aditya <adityaadiraj74@gmail.com>
2023-12-28 21:32:26 +0530

    q2 assignment

--- sc/day15.java
+++ sc/day15.java
@@ -5,14 +5,29 @@ import java.util.*;
 public class day15 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        // q1 Assignment ---
+        // int n = sc.nextInt();
+        // int count = 0;
+        // for (int i = 1; i <= n; i++) {
+        // if (n % i == 0) {
+        // count++;
+        // }
+        // }
+        // System.out.println(count);
+        // q2 Assignment ---
+        int a = sc.nextInt();
         int count = 0;
-        for (int i = 1; i <= n; i++) {
-            if (n % i == 0) {
+        for (int i = 1; i <= a; i++) {
+            if (a % i == 0) {
                 count++;
             }
         }
-        System.out.println(count);
+        if (count == 2) {
+            System.out.println("yes");
+        } else {
+            System.out.println("no");
+        }
+
         sc.close();
     }
 }
