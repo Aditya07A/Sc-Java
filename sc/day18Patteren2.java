10fe998936b5630c29bc397f5fae51a59cd2e96b
Aditya <adityaadiraj74@gmail.com>
2023-12-31 10:32:51 +0530

    q1 add

--- sc/day18Patteren2.java
+++ sc/day18Patteren2.java
@@ -69,6 +69,30 @@ public class day18Patteren2 {
         // System.out.println();
         // }
         // q5-Assignment----
+        // int n = sc.nextInt();
+        // for (int i = 1; i <= n; i++) {
+
+        // for (int j = 1; j <= n - i; j++) {
+        // System.out.print(" ");
+        // }
+        // for (int j = 1; j <= i; j++) {
+        // System.out.print("* ");
+        // }
+
+        // System.out.println();
+        // }
+        // q1-Add----
+        int N = sc.nextInt();
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                if (i == 1 || i == N || j == 1 || j == N) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
         sc.close();
 
     }
