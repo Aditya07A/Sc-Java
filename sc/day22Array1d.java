3073b0c0b4a7a8f1e169fe44976bba78466f51df
Aditya <adityaadiraj74@gmail.com>
2024-01-02 21:10:18 +0530

    upto q2

--- sc/day22Array1d.java
+++ sc/day22Array1d.java
@@ -16,17 +16,69 @@ public class day22Array1d {
         // System.out.print(a[i] + " ");
         // }
         // q2-Assignment----
-        int n = sc.nextInt();
-        int a[] = new int[n];
-        for (int i = 0; i < a.length; i++) {
-            // for (int i = 0; i < n; i++) {
-            a[i] = sc.nextInt();
-        }
-        for (int i = 0; i <= n - 1; i++) {
-            System.out.println(a[i] + " ");
-        }
-        Arrays.sort(a);
-        System.out.println(a[a.length - 1] + " &" + a[0]);
+        // int n = sc.nextInt();
+        // int a[] = new int[n];
+        // for (int i = 0; i < a.length; i++) {
+        // // for (int i = 0; i < n; i++) {
+        // a[i] = sc.nextInt();
+        // }
+        // for (int i = 0; i <= n - 1; i++) {
+        // System.out.println(a[i] + " ");
+        // }
+        // Arrays.sort(a);
+        // System.out.println(a[a.length - 1] + " &" + a[0]);
+
+        // int n = sc.nextInt();
+        // int a[] = new int[n];
+        // for (int i = 0; i < n; i++) {
+        // a[i] = sc.nextInt();
+        // }
+        // int min_arr = a[0];
+        // int max_arr = a[0];
+        // for (int i = 0; i < n; i++) {
+        // if (max_arr < a[i]) {
+        // max_arr = a[i];
+        // } else if (min_arr > a[i]) {
+        // min_arr = a[i];
+        // }
+        // }
+        // System.out.print(max_arr + " " + min_arr);
+
+        // static int maxOfArray(int[] a) {
+        // int n = a.length;
+        // int max = Integer.MIN_VALUE;
+        // for (int i = 0; i < n; i++) {
+        // if (a[i] > max) {
+        // max = a[i];
+        // }
+        // }
+        // return max;
+        // }
+
+        // static int minOfArray(int[] a) {
+        // int n = a.length;
+        // int min = Integer.MAX_VALUE;
+        // for (int i = 0; i < n; i++) {
+        // if (a[i] < min) {
+        // min = a[i];
+        // }
+        // }
+        // return min;
+        // }
+
+        // public static void main(String[] args) {
+        // Scanner sc = new Scanner(System.in);
+        // int n = sc.nextInt();
+        // int[] arr = new int[n];
+        // for (int i = 0; i < n; i++) {
+        // arr[i] = sc.nextInt();
+        // }
+        // sc.close();
+
+        // System.out.print(maxOfArray(arr) + " " + minOfArray(arr));
+        // }
+
+        // q3-Assignment----
 
         sc.close();
     }
