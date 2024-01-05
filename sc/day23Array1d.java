2e5364c82991c3c685a545996a3ff2c8401c1477
Aditya <adityaadiraj74@gmail.com>
2024-01-05 22:06:41 +0530

    q3

--- sc/day23Array1d.java
+++ sc/day23Array1d.java
@@ -2,27 +2,106 @@ package sc;
 
 import java.util.*;
 
+// public class day23Array1d {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+// q1-Assignment------
+// int T = sc.nextInt();
+// while (T > 0) {
+// int N = sc.nextInt();
+// int[] A = new int[N];
+// for (int i = 0; i < N; i++) {
+// A[i] = sc.nextInt();
+// }
+// int B = sc.nextInt();
+// int exists = 0;
+// for (int i = 0; i < N; i++) {
+// if (A[i] == B) {
+// exists = 1;
+// break;
+// }
+// }
+// System.out.println(exists);
+// T--;
+// }
+
+// ----with function
+// boolean searchArr(int[] arr, int B) {
+// int n = arr.length;
+// for (int i = 0; i < n; i++) {
+// int res = arr[i];
+// if (res == B) {
+// return true;
+// }
+// }
+// return false;
+// }
+
+//     sc.close();
+
+// }
+// q2-Assignment------
+// public class day23Array1d {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+
+//     int n = sc.nextInt();
+//     int[] a = new int[n];
+//     for (int i = 0; i < a.length; i++) {
+//         a[i] = sc.nextInt();
+//     }
+//     int b = sc.nextInt();
+//     int res = frequency(a, b);
+//     System.out.println("frequency of B:" + res);
+//     sc.close();
+
+// }
+
+// static int frequency(int[] arr, int B) {
+//     int n = arr.length;
+//     int count = 0;
+//     for (int i = 0; i < n; i++) {
+//         int res = arr[i];
+//         if (res == B) {
+//             count++;
+//         }
+//     }
+//     return count;
+// }
+// q3-Assignment------
+// public class day23Array1d {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int T = sc.nextInt();
+//         while (T > 0) {
+//             int N = sc.nextInt();
+//             int[] A = new int[N];
+//             for (int i = 0; i < N; i++) {
+//                 A[i] = sc.nextInt();
+//             }
+//             System.out.println();
+//             for (int i = 0; i < N; i++) {
+//                 if (A[i] % 2 == 0) {
+//                     System.out.println(A[i] + " ");
+//                 }
+//             }
+//             System.out.println();
+//             for (int i = 0; i < N; i++) {
+//                 if (A[i] % 2 == 1) {
+//                     System.out.println(A[i] + " ");
+//                 }
+//             }
+//             T--;
+//         }
+//         sc.close();
+//     }
+
+// }
+// q4-Assignment-----
 public class day23Array1d {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
-        while (T > 0) {
-            int N = sc.nextInt();
-            int[] A = new int[N];
-            for (int i = 0; i < N; i++) {
-                A[i] = sc.nextInt();
-            }
-            int B = sc.nextInt();
-            int exists = 0;
-            for (int i = 0; i < N; i++) {
-                if (A[i] == B) {
-                    exists = 1;
-                    break;
-                }
-            }
-            System.out.println(exists);
-            T--;
-        }
+
         sc.close();
     }
 
