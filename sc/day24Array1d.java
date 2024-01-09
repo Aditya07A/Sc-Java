ec86534ce1c5378dbf435a8a710a769dce6a07a1
Aditya <adityaadiraj74@gmail.com>
2024-01-09 21:11:48 +0530

    q3

--- sc/day24Array1d.java
+++ sc/day24Array1d.java
@@ -1,66 +1,106 @@
 package sc;
 
+// import java.util.*;
+
+// public class day24Array1d {
+
+// // q1-Assignment----
+// public static int[] frequency(int[] a) {
+// int n = a.length;
+// int[] res = new int[n];
+// for (int i = 0; i <= n - 1; i++) {
+// int count = 0;
+// for (int j = 0; j <= n - 1; j++) {
+// if (a[j] == a[i]) {
+// count++;
+// }
+// }
+
+// res[i] = count;
+// }
+// return res;
+// }
+
+// public static void main(String[] args) {
+
+// Scanner sc = new Scanner(System.in);
+
+// int n = sc.nextInt();
+
+// int[] a = new int[n];
+// for (int i = 0; i < a.length; i++) {
+// a[i] = sc.nextInt();
+// }
+// int[] output = frequency(a);
+// for (int i = 0; i < output.length; i++) {
+// System.out.print(output[i] + " ");
+// }
+
+// sc.close();
+
+// }
+// // q2-Assignment----
+// public static int unique(int[] a) {
+
+// int ans = 0;
+// for (int i = 0; i < a.length; i++) {
+// int count = 0;
+// for (int j = 0; j < a.length; j++) {
+// if (a[j] == a[i]) {
+// count++;
+// }
+// }
+// if (count == 1) {
+// ans = a[i];
+// return ans;
+// }
+
+// }
+// return -1;
+// }
+
+// public static void main(String[] args) {
+
+// Scanner sc = new Scanner(System.in);
+
+// int n = sc.nextInt();
+
+// int[] a = new int[n];
+// for (int i = 0; i < a.length; i++) {
+// a[i] = sc.nextInt();
+// }
+// int output = unique(a);
+
+// if (output != -1) {
+// System.out.println("Unique element: " + output);
+// } else {
+// System.out.println("No unique element found.");
+// }
+
+// sc.close();
+
+// }
 import java.util.*;
+
+// public class day24Array1d {
+// q3-Assignment----
+import java.util.Scanner;
 
 public class day24Array1d {
 
-    // // q1-Assignment----
-    // public static int[] frequency(int[] a) {
-    // int n = a.length;
-    // int[] res = new int[n];
-    // for (int i = 0; i <= n - 1; i++) {
-    // int count = 0;
-    // for (int j = 0; j <= n - 1; j++) {
-    // if (a[j] == a[i]) {
-    // count++;
-    // }
-    // }
-
-    // res[i] = count;
-    // }
-    // return res;
-    // }
-
-    // public static void main(String[] args) {
-
-    // Scanner sc = new Scanner(System.in);
-
-    // int n = sc.nextInt();
-
-    // int[] a = new int[n];
-    // for (int i = 0; i < a.length; i++) {
-    // a[i] = sc.nextInt();
-    // }
-    // int[] output = frequency(a);
-    // for (int i = 0; i < output.length; i++) {
-    // System.out.print(output[i] + " ");
-    // }
-
-    // sc.close();
-
-    // }
-    // q2-Assignment----
-    public static int unique(int[] a) {
-
-        int ans = 0;
-        for (int i = 0; i < a.length; i++) {
-            int count = 0;
-            for (int j = 0; j < a.length; j++) {
-                if (a[j] == a[i]) {
-                    count++;
+    static class Solution {
+        public int solve(int[] A) {
+            int n = A.length;
+            for (int i = 0; i <= n - 2; i++) {
+                if (A[i] > A[i + 1]) {
+                    return 0;
                 }
             }
-            if (count == 1) {
-                ans = a[i];
-                return ans;
-            }
-
+            return 1;
         }
-        return -1;
     }
 
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
@@ -69,16 +109,13 @@ public class day24Array1d {
         for (int i = 0; i < a.length; i++) {
             a[i] = sc.nextInt();
         }
-        int output = unique(a);
 
-        if (output != -1) {
-            System.out.println("Unique element: " + output);
-        } else {
-            System.out.println("No unique element found.");
-        }
+        Solution solution = new Solution();
+        int result = solution.solve(a);
+        System.out.println("The array is sorted: " + (result == 1));
 
         sc.close();
-
     }
-
 }
+
+// }
