09efa85ba3c30b75c330ab3042798f24fee244b9
Aditya <adityaadiraj74@gmail.com>
2024-01-13 20:55:26 +0530

    q2

--- sc/day24Array1d.java
+++ sc/day24Array1d.java
@@ -87,34 +87,34 @@ package sc;
 
 // public class day24Array1d {
 
-//     static class Solution {
-//         public int solve(int[] A) {
-//             int n = A.length;
-//             for (int i = 0; i <= n - 2; i++) {
-//                 if (A[i] > A[i + 1]) {
-//                     return 0;
-//                 }
-//             }
-//             return 1;
-//         }
-//     }
+// static class Solution {
+// public int solve(int[] A) {
+// int n = A.length;
+// for (int i = 0; i <= n - 2; i++) {
+// if (A[i] > A[i + 1]) {
+// return 0;
+// }
+// }
+// return 1;
+// }
+// }
 
-//     public static void main(String[] args) {
-//         Scanner sc = new Scanner(System.in);
+// public static void main(String[] args) {
+// Scanner sc = new Scanner(System.in);
 
-//         int n = sc.nextInt();
+// int n = sc.nextInt();
 
-//         int[] a = new int[n];
-//         for (int i = 0; i < a.length; i++) {
-//             a[i] = sc.nextInt();
-//         }
+// int[] a = new int[n];
+// for (int i = 0; i < a.length; i++) {
+// a[i] = sc.nextInt();
+// }
 
-//         Solution solution = new Solution();
-//         int result = solution.solve(a);
-//         System.out.println("The array is sorted: " + (result == 1));
+// Solution solution = new Solution();
+// int result = solution.solve(a);
+// System.out.println("The array is sorted: " + (result == 1));
 
-//         sc.close();
-//     }
+// sc.close();
+// }
 // }
 // // q4-Assignment----
 
@@ -122,121 +122,98 @@ package sc;
 
 // public class day24Array1d {
 
-//     public static class Solution {
-//         public int solve(int[] A, int B) {
-//             int n = A.length;
-//             int count = 0;
-//             for (int i = 0; i < n; i++) {
-//                 for (int j = i + 1; j < n; j++) {
-//                     if (A[i] + A[j] == B) {
-//                         count++;
-//                     }
-//                 }
-//             }
-//             return count;
-//         }
-//     }
+// public static class Solution {
+// public int solve(int[] A, int B) {
+// int n = A.length;
+// int count = 0;
+// for (int i = 0; i < n; i++) {
+// for (int j = i + 1; j < n; j++) {
+// if (A[i] + A[j] == B) {
+// count++;
+// }
+// }
+// }
+// return count;
+// }
+// }
 
-//     public static void main(String[] args) {
-//         Scanner sc = new Scanner(System.in);
+// public static void main(String[] args) {
+// Scanner sc = new Scanner(System.in);
 
-//         int n = sc.nextInt();
+// int n = sc.nextInt();
 
-//         int[] a = new int[n];
-//         for (int i = 0; i < a.length; i++) {
-//             a[i] = sc.nextInt();
-//         }
+// int[] a = new int[n];
+// for (int i = 0; i < a.length; i++) {
+// a[i] = sc.nextInt();
+// }
 
-//         int valueB = sc.nextInt();
+// int valueB = sc.nextInt();
 
-//         Solution solution = new Solution();
-//         int countOfPairs = solution.solve(a, valueB);
+// Solution solution = new Solution();
+// int countOfPairs = solution.solve(a, valueB);
 
-//         System.out.println("The count of pairs whose sum is " + valueB + " is: " + countOfPairs);
+// System.out.println("The count of pairs whose sum is " + valueB + " is: " +
+// countOfPairs);
 
-//         sc.close();
-//     }
+// sc.close();
+// }
 // }
 
 import java.util.Scanner;
 
 // public class day24Array1d {
-//     public int[] solve(int[] A) {
-//         int n = A.length;
-//         int inverse[] = new int[n];
+// public int[] solve(int[] A) {
+// int n = A.length;
+// int inverse[] = new int[n];
 
-//         for (int i = 0; i < n; i++) {
-//             inverse[A[i]] = i;
-//         }
-//         return inverse;
-//     }
+// for (int i = 0; i < n; i++) {
+// inverse[A[i]] = i;
+// }
+// return inverse;
+// }
 
-//     public static void main(String args[]) {
-//         try (Scanner scn = new Scanner(System.in)) {
-//             int n = scn.nextInt();
-//             int a[] = new int[n];
+// public static void main(String args[]) {
+// try (Scanner scn = new Scanner(System.in)) {
+// int n = scn.nextInt();
+// int a[] = new int[n];
 
-//             for (int i = 0; i < n; i++) {
-//                 a[i] = scn.nextInt();
-//             }
+// for (int i = 0; i < n; i++) {
+// a[i] = scn.nextInt();
+// }
 
-//             day24Array1d solution = new day24Array1d();
-//             int[] result = solution.solve(a);
+// day24Array1d solution = new day24Array1d();
+// int[] result = solution.solve(a);
 
-//             System.out.println("Inverse array:");
-//             for (int value : result) {
-//                 System.out.print(value + " ");
-//             }
-//         }
-//     }
+// System.out.println("Inverse array:");
+// for (int value : result) {
+// System.out.print(value + " ");
+// }
+// }
+// }
 // }
 
 // public class day24Array1d {
-//     public static int[] rightshift(int[] a) {
-//         int[] b = new int[a.length];
-//         b[0] = a[a.length - 1];
+// public static int[] rightshift(int[] a) {
+// int[] b = new int[a.length];
+// b[0] = a[a.length - 1];
 
-//         for (int i = 0; i < a.length - 1; i++) {
-//             b[i + 1] = a[i];
-//         }
-//         return b;
-//     }
-
-//     public static void main(String[] args) {
-//         Scanner sc = new Scanner(System.in);
-//         int n = sc.nextInt();
-//         int a[] = new int[n];
-//         for (int i = 0; i < n; i++) {
-//             a[i] = sc.nextInt();
-//         }
-//         int shifted[] = rightshift(a);
-//         for (int i = 0; i < n; i++) {
-//             System.out.print(shifted[i] + " ");
-//         }
-//         sc.close();
-//     }
+// for (int i = 0; i < a.length - 1; i++) {
+// b[i + 1] = a[i];
 // }
-//dumy
-public class day23Array1d {
-    public static boolean solve(int[] a) {
-        for (int i = 0; i < a.length; i++) {
-            if (a[i] == a[i + 1]) {
-                return true;
-            }
-        }
-        return false;
-    }
+// return b;
+// }
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] a = new int[n];
-        for (int i = 0; i < a.length; i++) {
-            a[i] = sc.nextInt();
-        }
-
-        System.out.println("result : " + solve(a));
-
-        sc.close();
-    }
-}
+// public static void main(String[] args) {
+// Scanner sc = new Scanner(System.in);
+// int n = sc.nextInt();
+// int a[] = new int[n];
+// for (int i = 0; i < n; i++) {
+// a[i] = sc.nextInt();
+// }
+// int shifted[] = rightshift(a);
+// for (int i = 0; i < n; i++) {
+// System.out.print(shifted[i] + " ");
+// }
+// sc.close();
+// }
+// }
--- sc/day252D.java
+++ sc/day252D.java
@@ -6,18 +6,39 @@ public class day252D {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         // q1-Assignment
-        int n = sc.nextInt();
-        int m = sc.nextInt();
+        // int n = sc.nextInt();
+        // int m = sc.nextInt();
 
-        int[][] mat = new int[n][m];
-        for (int i = 0; i <= n - 1; i++) {
-            // System.out.print("Row " + (i + 1) + ": "); for show row number
-            for (int j = 0; j <= m - 1; j++) {
-                mat[i][j] = sc.nextInt();
-                System.out.println(mat[i][j] + " ");
+        // int[][] mat = new int[n][m];
+        // for (int i = 0; i <= n - 1; i++) {
+        //     // System.out.print("Row " + (i + 1) + ": "); for show row number
+        //     for (int j = 0; j <= m - 1; j++) {
+        //         mat[i][j] = sc.nextInt();
+        //         System.out.println(mat[i][j] + " ");
+        //     }
+        //     System.out.println();
+        // }
+        //q2-Assignment------
+        int N = scn.nextInt();
+        int M = scn.nextInt();
+      
+        int[][]mat = new int[N][M];
+      
+        for(int i=0; i < N;i++) {
+            for(int j=0; j < M;j++) {
+               mat[i][j] = scn.nextInt();
+            }
+        }
+      
+      
+        //print col by col
+        for(int j=0;j < M;j++) {
+            for(int i=0;i < N;i++) {
+               System.out.print(mat[i][j] + " ");
             }
             System.out.println();
         }
-        sc.close();
+
     }
-}
+        sc.close();
+    }}
