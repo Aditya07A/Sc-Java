486b550f1ca40524ef53b5a499f052d46c9fd8a0
Aditya <adityaadiraj74@gmail.com>
2024-01-21 21:03:30 +0530

    demo2

--- demo1.java
+++ demo1.java
@@ -1,5 +1,28 @@
 
-class Calculator {
+// class Calculator {
+//     int a;
+
+//     public int add(int n1, int n2) {
+
+//         int r = n1 + n2;
+//         return r;
+//     }
+
+// }
+
+// public class demo1 {
+
+//     public static void main(String[] args) {
+//         int num1 = 4;
+//         int num2 = 5;
+
+//         Calculator calc = new Calculator();
+//         int res = calc.add(num1, num2);
+//         System.out.println(res);
+
+//     }
+// }
+class Calculator1 {
     int a;
 
     public int add(int n1, int n2) {
@@ -16,7 +39,7 @@ public class demo1 {
         int num1 = 4;
         int num2 = 5;
 
-        Calculator calc = new Calculator();
+        Calculator1 calc = new Calculator1();
         int res = calc.add(num1, num2);
         System.out.println(res);
 
