b382980c8ba03c9f8fe057e191e3ac0d4ae67b31
Aditya <adityaadiraj74@gmail.com>
2024-01-23 21:04:30 +0530

    try

--- sc/demo1.java
+++ sc/demo1.java
@@ -62,24 +62,38 @@ package sc;
 import java.util.Scanner;
 
 public class demo1 {
+    // public static void main(String[] args) {
+    // Scanner sc = new Scanner(System.in);
+    // int N = sc.nextInt();
+    // int k = N - 1;
+    // for (int i = 1; i < 2 * N; i++) {
+    // if (i <= N) {
+    // for (int j = 1; j <= i; j++) {
+    // System.out.print("*");
+    // }
+    // } else {
+    // for (int j = 1; j <= k; j++) {
+    // System.out.print("*");
+    // }
+    // k = k - 1;
+    // }
+    // System.out.println();
+    // }
+    // sc.close();
+
+    // }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int k = N - 1;
-        for (int i = 1; i < 2 * N; i++) {
-            if (i <= N) {
-                for (int j = 1; j <= i; j++) {
-                    System.out.print("*");
-                }
-            } else {
-                for (int j = 1; j <= k; j++) {
-                    System.out.print("*");
-                }
-                k = k - 1;
+        // q1-Assignment----
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            System.out.print("*");
+            for (int j = 1; j <= n; j++) {
+                System.out.print(" ");
             }
+            System.out.print("*");
             System.out.println();
         }
         sc.close();
-
     }
 }
